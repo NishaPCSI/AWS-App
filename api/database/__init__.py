d9d14414810a207eea3c6eb5e1d8907d29d78fb4
DipeshMajithiaPCSI <120078843+DipeshMajithiaPCSI@users.noreply.github.com>
2025-05-05 12:57:30 +0530

    done

--- api/database/__init__.py
+++ api/database/__init__.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from typing import Dict
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# -------------------------------------Database URL Configuration---------------------------------------
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = "postgresql://Group42_admin:Nisarg#123@group42-database.c5iq0gm14ozl.us-east-1.rds.amazonaws.com:5432/Group42_Database"
+
+# -------------------------------------Engine Setup---------------------------------------
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# -------------------------------------Session and Base---------------------------------------
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -1,6 +1,6 @@
 import type { User } from "../types/User"
 import { writable, type Writable } from "svelte/store"
 
-export const API_ENDPOINT = writable("http://127.0.0.1:8000")
+export const API_ENDPOINT = writable("http://13.218.46.83:8000")
 
 export const USER_DATA:Writable<User> = writable()
